d974a587d9c1186ef508fc6d20208c6f09640372
Luiz Augusto <luizxdas@outlook.com>
2024-12-09 11:54:45 -0300

    Remove o lombok pois estava dando erro no mvn clean package

--- src/main/java/com/biblioteca/Biblioteca/entities/Book.java
+++ src/main/java/com/biblioteca/Biblioteca/entities/Book.java
@@ -2,24 +2,17 @@ package com.biblioteca.Biblioteca.entities;
 
 import com.biblioteca.Biblioteca.controller.dto.BookRequestDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Table(name = "books")
 @Entity(name = "books")
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode(of = "id")
 public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
     private String image;
     private String author;
@@ -27,12 +20,81 @@ public class Book {
     private Integer year;
     private String synopsis;
 
-    public Book (BookRequestDTO data) {
+    public Book() {
+    }
+
+    public Book(Long id, String title, String image, String author, List<String> genres, Integer year, String synopsis) {
+        this.id = id;
+        this.title = title;
+        this.image = image;
+        this.author = author;
+        this.genres = genres;
+        this.year = year;
+        this.synopsis = synopsis;
+    }
+
+    public Book(BookRequestDTO data) {
         this.title = data.title();
         this.image = data.image();
         this.author = data.author();
         this.genres = data.genres();
         this.year = data.year();
         this.synopsis = data.synopsis();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setGenres(List<String> genres) {
+        this.genres = genres;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
     }
 }
--- src/main/java/com/biblioteca/Biblioteca/entities/Credits.java
+++ src/main/java/com/biblioteca/Biblioteca/entities/Credits.java
@@ -1,19 +1,10 @@
 package com.biblioteca.Biblioteca.entities;
 
-import com.biblioteca.Biblioteca.controller.dto.BookRequestDTO;
 import com.biblioteca.Biblioteca.controller.dto.CreditsDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Table(name = "credits")
 @Entity(name = "credits")
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode(of = "book_id")
 public class Credits {
 
     @Id
@@ -26,13 +17,82 @@ public class Credits {
     private String text_license;
     private Boolean modified;
 
+    public Credits() {
+    }
+
+    public Credits(Integer book_id, String image_source, String image_author, String image_license, String text_source, String text_license, Boolean modified) {
+        this.book_id = book_id;
+        this.image_source = image_source;
+        this.image_author = image_author;
+        this.image_license = image_license;
+        this.text_source = text_source;
+        this.text_license = text_license;
+        this.modified = modified;
+    }
+
     public Credits(CreditsDTO data) {
         this.book_id = data.book_id();
         this.image_source = data.image_source();
         this.image_author = data.image_author();
         this.image_license = data.image_license();
-        this.text_source= data.text_source();
+        this.text_source = data.text_source();
         this.text_license = data.text_license();
         this.modified = data.modified();
+    }
+
+    public Integer getBook_id() {
+        return book_id;
+    }
+
+    public String getImage_source() {
+        return image_source;
+    }
+
+    public String getImage_author() {
+        return image_author;
+    }
+
+    public String getImage_license() {
+        return image_license;
+    }
+
+    public String getText_source() {
+        return text_source;
+    }
+
+    public String getText_license() {
+        return text_license;
+    }
+
+    public Boolean getModified() {
+        return modified;
+    }
+
+    public void setBook_id(Integer book_id) {
+        this.book_id = book_id;
+    }
+
+    public void setImage_source(String image_source) {
+        this.image_source = image_source;
+    }
+
+    public void setImage_author(String image_author) {
+        this.image_author = image_author;
+    }
+
+    public void setImage_license(String image_license) {
+        this.image_license = image_license;
+    }
+
+    public void setText_source(String text_source) {
+        this.text_source = text_source;
+    }
+
+    public void setText_license(String text_license) {
+        this.text_license = text_license;
+    }
+
+    public void setModified(Boolean modified) {
+        this.modified = modified;
     }
 }
